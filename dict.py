a5ed2f9e00827bb915909f23867153c363f18577
saqlainraza03 <62935051+saqlainraza03@users.noreply.github.com>
2020-05-20 12:28:56 +0530

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,8 @@
+#create a dict and take input from the3 user and return the meaning
+#of the word from the dictionary.
+
+dict={"sasta":"1200","mehnga":1500,"raabta":"3200",
+    "pinn":"510230"}
+
+name=input("enter the name : \n ")
+print(dict[name])
